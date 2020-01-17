cd27265d795801e6d61e8aeb837fc82a878516fc
k-washi <kai.washizaki@gmail.com>
2020-01-17 22:51:14 +0900

    Wrapped error

---
+++ errorhandling/errors_pkg/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/k-washi/golang-cookbook/errorhandling/errors_pkg/errwrap"
+)
+
+func main() {
+	errwrap.Wrap()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/k-washi/golang-cookbook
 
 go 1.12
+
+require (
+	github.com/pkg/errors v0.9.1
+	gopkg.in/errgo.v2 v2.1.0
+)
